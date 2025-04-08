6a4d134eab24c03597da60d6503ae7a9667306bc
alxate <juan.jpav.79@gmail.com>
2025-04-08 09:02:47 -0500

    se centralizo el Register

--- React-Alzate/src/App.tsx
+++ React-Alzate/src/App.tsx
@@ -3,7 +3,7 @@ import Register from "./components/Register";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div style={{ textAlign: 'center' }}>
       <h1>Registro de usuario</h1>
       <Register />
     </div>
